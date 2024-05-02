75b3cabdd80e311dc361db424687f3c3a3c39927
ma-suwa <rspturn@gmail.com>
2024-05-03 01:53:41 +0900

    ver2
    デザイン反映

--- main.js
+++ main.js
@@ -1,140 +1,256 @@
+document.addEventListener('DOMContentLoaded', () => {
 
-let image;
+    let image;
 
-window.addEventListener('load', init);
-function init(){
-
-  //分割パラメーター
-  const PARAMS = {
-    split: 3,
-    offset: 0
-  };
-  const pane = new Tweakpane.Pane();
-
-  pane.addInput(PARAMS, 'split',{min: 0, max: 10, step: 1})
-  .on('change', (ev) => {
-    if(ev.last){
-      //img全削除
-      allImgRemove();
-      saidan(image,PARAMS.split,PARAMS.offset);
+    const alertElement = document.getElementById('alert');
+    function alertMessage(message) {
+        if(alertElement) {
+            alertElement.style.display = 'block';
+            alertElement.innerText = message;
+        }
+        // setTimeout(() => {
+        //     alertElement.style.display = 'none';
+        // }, 10000);
     }
-  });
 
-  pane.addInput(PARAMS, 'offset',{min: -2000, max: 2000, step: 1})
-  .on('change', (ev) => {
-    if(ev.last){
-      //img全削除
-      allImgRemove();
-      saidan(image,PARAMS.split,PARAMS.offset);
-    }
-  });
+    const PARAMS = {
+        split: 3
+    };
+    const pane = new Tweakpane.Pane();
+    pane.addInput(PARAMS, 'split', { min: 0, max: 15, step: 1 })
+        .on('change', (ev) => {
+            if (ev.last) {
+                allImgRemove();
+                saidan(image, PARAMS.split);
+            }
+        });
 
-  //ドラッグオンドロップ
-  let container = document.getElementById('imgBox');
-	let dropZone = document.getElementById('drop-zone');
-  container.addEventListener('dragover', () => {
-    dropZone.style.zIndex =99;
-    container.style.zIndex = 1;
-	});
-	dropZone.addEventListener('dragover', (event) => {
-    console.log("dragover");
-		event.stopPropagation();
-		event.preventDefault();
-		event.dataTransfer.dropEffect = 'copy';
-    dropZone.style.opacity = 0.8;
-	});
-	dropZone.addEventListener('drop', (event) => {
-    console.log("drop");
-		event.stopPropagation();
-		event.preventDefault();
-		const fileList = event.dataTransfer.files;
-		loadLocalImage(fileList, PARAMS);
+
+
+    const Application = PIXI.Application;
+    const Loader = PIXI.Loader.shared;
+    const Texture = PIXI.Texture;
+    const Sprite = PIXI.Sprite;
+
+    const dropZone = document.getElementById('drop-zone');
+    dropZone.addEventListener('dragover', (event) => {
+        dropZone.style.backgroundColor = '#d8ddea';
+        event.stopPropagation();
+        event.preventDefault();
+        event.dataTransfer.dropEffect = 'copy';
+    });
+    dropZone.addEventListener('drop', (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        const fileList = event.dataTransfer.files;
+        loadLocalImage(fileList);
+        dropZone.style.display = 'none';
+        imgBox.style.display = 'block';
+    });
+
+    const imgBox = document.getElementById('imgBox');
+    imgBox.addEventListener('dragover', (event) => { 
+        console.log('dragover');
+        dropZone.style.display = 'block';
+        imgBox.style.display = 'none';
+    });
+
+    function loadLocalImage(files) {
+        const fileData = files[0];
+        if (!fileData.type.match('image.*')) {
+            alert('画像を選択してください');
+            return;
+        }
     
-    dropZone.style.opacity = 0;
-    dropZone.style.zIndex =-1;
-    container.style.zIndex = 1;
-    console.log( container.style.zIndex, dropZone.style.zIndex)
-	});
-
-
-}
-
-function loadLocalImage(e, PARAMS) {
-
-  //img全削除
-  allImgRemove();
-
-	let fileData = e[0];
-
-	// 画像ファイル以外は処理を止める
-	if(!fileData.type.match('image.*')) {
-		alert('画像を選択してください');
-		return;
-	}
-
-	// FileReaderオブジェクトを使ってファイル読み込み
-	var reader = new FileReader();
-	// ファイル読み込みに成功したときの処理
-	reader.onload = function() {
-    image = new Image();
-    image.crossOrigin= 'Anonymous';
-    image.src= reader.result;
+        const reader = new FileReader();
+        reader.onload = function () {
+            alertMessage("読み込んでいます...");
     
-    //画像読み込んだら裁断
-    image.onload= function(){
-        saidan(image,PARAMS.split,PARAMS.offset);
+            let imageSrc = reader.result;
+            const img = new Image();
+            img.onload = function() {
+                const maxHeight = 15000; // Set the maximum height for the image
+                let width = img.width;
+                let height = img.height;
+    
+                // Check if the image exceeds the maximum height
+                if (height > maxHeight) {
+                    alertMessage("画像サイズが大きすぎるためリサイズしています...");
+    
+                    const aspectRatio = width / height;
+                    height = maxHeight;
+                    width = height * aspectRatio;
+    
+                    const canvas = document.createElement('canvas');
+                    const ctx = canvas.getContext('2d');
+                    canvas.width = width;
+                    canvas.height = height;
+                    // Draw the resized image on the canvas
+                    ctx.drawImage(img, 0, 0, width, height);
+                    // Convert the canvas to a data URL
+                    const resizedImageSrc = canvas.toDataURL('image/jpeg');
+                    imageSrc = resizedImageSrc;
+                }
+    
+                // Reset Loader to remove existing resources
+                Loader.reset();
+    
+                Loader.add('image', imageSrc)
+                    .load((loader, resources) => {
+                        alertElement.style.display = 'none';
+                        image = resources.image.texture;
+                        allImgRemove(); // Remove existing images
+                        saidan(image, PARAMS.split); // Call saidan() with the new image
+                        // dropZone.style.display = 'none';
+                        document.getElementById('imgBox').style.display = 'flex';
+                    });
+            };
+            img.src = imageSrc;
+        };
+        reader.readAsDataURL(fileData);
     }
-	}
-	// ファイル読み込みを実行
-	reader.readAsDataURL(fileData);
+    
 
-}
+    let app = [];
+    let saidanCanvasList = [];
+    let buttonList = [];
 
+    function saidan(image, split) {
+        const spriteHeight = image.height / split;
+        
+        for (let i = 0; i < split; i++) {
+            app[i] = new Application({ transparent: true });
 
-//裁断処理
-function saidan(image,split,offset){
-    const sprite= {
-        width:image.width,
-        height:image.height/split
-      }
-    const canvas = document.createElement('canvas');
-    canvas.width = sprite.width;
-    canvas.height = sprite.height;
-  
-    const context= canvas.getContext('2d')
-      //drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
-    for(let i=0; i*sprite.height < image.height; i++){
-      for(let j=0; j*sprite.width < image.width; j++){
+            saidanCanvasList[i] = document.createElement('div');
+            saidanCanvasList[i].classList.add('saidanCanvas');
+            document.getElementById('imgBox').appendChild(saidanCanvasList[i]);
 
-            context.drawImage(
-              image,
-              j*sprite.width,
-              i*sprite.height-offset*split,
-              sprite.width,
-              sprite.height,
-              0,
-              0,
-              sprite.width,
-              sprite.height
-            )
+            buttonList[i] = document.createElement('button');
+            buttonList[i].classList.add('copyButton');
 
-        let spriteElement = new Image();
-        spriteElement.src = canvas.toDataURL('jpg');
-        spriteElement.download = "sample.jpg";
+            const texture = new Texture(image.baseTexture);
+            texture.frame = new PIXI.Rectangle(0, 0, image.width, image.height);
+            app[i].renderer.resize(image.width, spriteHeight);
+            let sprite = new Sprite(texture);
+            sprite.y = -i * spriteHeight;
+            sprite.interactive = true;
+            sprite.buttonMode = true;
 
-  
-        document.querySelector('#imgBox').appendChild(spriteElement)
-      }
+            sprite.on('pointerdown', onDragStart);
+            sprite.on('pointerup', onDragEnd);
+            sprite.on('pointerupoutside', onDragEnd);
+            sprite.on('pointermove', onDragMove);
+
+            app[i].stage.addChild(sprite);
+        }
+        setPlusButton();
+        setCopyButton();
     }
-      
-}
 
-function allImgRemove(){
-    //img全削除
-    const parent = document.getElementById('imgBox');
-    while(parent.firstChild){
-      parent.removeChild(parent.firstChild);
+    function setCopyButton() {
+        for (let i = 0; i < app.length; i++) {
+            let saidanCanvas = saidanCanvasList[i];
+            let button = buttonList[i];
+    
+            button.removeEventListener('click', buttonClickHandler);
+    
+            saidanCanvas.appendChild(app[i].view);
+            app[i].renderer.render(app[i].stage);  
+    
+            saidanCanvas.addEventListener('mouseenter', () => {
+                alertMessage("画像をつかんで上下にドラッグできます!");
+                saidanCanvas.appendChild(button);
+            });
+            saidanCanvas.addEventListener('mouseleave', () => {
+                if (button.parentNode === saidanCanvas) {
+                    saidanCanvas.removeChild(button);
+                }
+            });
+
+            button.addEventListener('mouseenter', () => {
+                alertMessage("COPYボタンをクリックすると画像をコピーできます!");
+            });
+            button.addEventListener('click', buttonClickHandler);
+        }
     }
-}
 
+    function buttonClickHandler() {
+        let index = buttonList.indexOf(this);
+        let imageBase64 = app[index].renderer.extract.base64();
+        copyImageToClipboard(app[index], imageBase64);
+    }
 
+    function copyImageToClipboard(app, imageBase64) {
+        const img = new Image();
+        img.src = imageBase64;
+        img.onload = function () {
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+            const width = app.renderer.width;
+            const height = app.renderer.height;
+            canvas.width = width;
+            canvas.height = height;
+            ctx.drawImage(img, 0, 0, width, height);
+    
+            canvas.toBlob((blob) => {
+                const item = new ClipboardItem({ 'image/png': blob });
+                navigator.clipboard.write([item]).then(() => {
+                    alertMessage("コピーが完了しました！");
+                }).catch((error) => {
+                    console.error('Failed to copy image to clipboard:', error);
+                });
+            });
+        };
+    }
+
+    function setPlusButton(){
+        const plusButton = document.getElementsByClassName('plusButton')[0];
+        if (plusButton) {
+            plusButton.style.display = 'block';
+            plusButton.addEventListener('mouseenter', () => {
+                alertMessage("↗︎ 右上のメニューからでも分割数を増やせます!");
+            });
+            plusButton.addEventListener('click', plusSplit);
+        }
+    }
+
+    function plusSplit(){
+        PARAMS.split++;
+        pane.refresh();
+    }
+
+    function onDragStart(event) {
+        this.dragData = event.data;
+        this.dragging = true;
+        this.dragPoint = event.data.getLocalPosition(this.parent);
+        const copyButtons = document.getElementsByClassName('copyButton');
+        for (let i = 0; i < copyButtons.length; i++) {
+            copyButtons[i].style.opacity = 0;
+        }
+    }
+
+    function onDragEnd() {
+        this.dragging = false;
+        this.dragData = null;
+        setCopyButton();
+        const copyButtons = document.getElementsByClassName('copyButton');
+        for (let i = 0; i < copyButtons.length; i++) {
+            copyButtons[i].style.opacity = 1;
+        }
+    }
+
+    function onDragMove(event) {
+        if (this.dragging) {
+            const newPosition = this.dragData.getLocalPosition(this.parent);
+            this.position.y += (newPosition.y - this.dragPoint.y);
+            this.dragPoint = newPosition;
+        }
+    }
+
+    function allImgRemove() {
+        const imgBox = document.getElementById('imgBox');
+        while (imgBox.firstChild) {
+            imgBox.removeChild(imgBox.firstChild);
+        }
+    }
+});
